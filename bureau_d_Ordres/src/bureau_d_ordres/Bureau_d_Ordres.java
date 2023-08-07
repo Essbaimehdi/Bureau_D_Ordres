10155d2864cededae0eeb14cc784756b72d70a1d
elmeh <elmeh@192.168.1.12>
2023-08-07 18:49:46 +0200

    CommitThreeCommit

---
+++ bureau_d_Ordres/src/bureau_d_ordres/Bureau_d_Ordres.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bureau_d_ordres;
+
+/**
+ *
+ * @author elmeh
+ */
+public class Bureau_d_Ordres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ bureau_d_Ordres/src/bureau_d_ordres/Connectionz.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bureau_d_ordres;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author elmeh
+ */
+public class Connectionz {
+   static  Connection conn;
+   public static Connection getConnection(){
+       try{
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/etudiant","root","");
+       }catch(Exeption ex){
+           
+       }
+   }
+}
